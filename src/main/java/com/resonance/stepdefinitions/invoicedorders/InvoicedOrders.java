546ddb9e7d3cd1ad6a15bb3ca9e1669e7f3a1e1b
shucrushder <shu71294@gmail.com>
2022-11-22 15:16:37 +0530

    commit

--- src/main/java/com/resonance/stepdefinitions/invoicedorders/InvoicedOrders.java
+++ src/main/java/com/resonance/stepdefinitions/invoicedorders/InvoicedOrders.java
@@ -1,6 +1,29 @@
 package com.resonance.stepdefinitions.invoicedorders;
 import org.testng.asserts.*;
 
+import com.resonance.pages.HomePage;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class InvoicedOrders {
-   
+   @Given ("User logs into the application as manager")
+   public void loginToTheApplication() {
+		HomePage home = new HomePage();
+		home.clickOnLogin();
+		home.enterUserName("manager");
+		home.enterPassword("12345678");
+		home.clickOnLogin();
+	}
+      @When ("manager selects Invoiced order from picking list")
+     public void SelectInvoicedOrder() {
+		
+
+	}
+      @Then ("manager should see the list of Invoiced details")
+      public void InvoicedList() {
+		
+
+	}
 }
---
+++ src/test/resources/Features/InvoicedOrders.feature
@@ -0,0 +1,11 @@
+Feature: This Feature contains test case of Invoiced orders
+
+
+
+
+Scenario: 
+
+ Given User logs into the application as manager
+ When manager selects Invoiced order from picking list
+ Then manager should see the list of Invoiced details
+
